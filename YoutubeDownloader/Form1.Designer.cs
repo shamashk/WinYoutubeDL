54d1ba339fc73bfb8982d0751b636b92a51d581c
shamashk <shamashk01@gmail.com>
2016-03-10 23:54:19 +0530

    Initial check-in of module WinYoutubeDL

---
+++ YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.clipboardMonitor1 = new YoutubeDownloader.ClipboardMonitor();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(403, 20);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(423, 9);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 226);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(486, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(12, 47);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(60, 13);
+            this.lblDestination.TabIndex = 4;
+            this.lblDestination.Text = "Destination";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(78, 44);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(337, 20);
+            this.txtDestination.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            // 
+            // clipboardMonitor1
+            // 
+            this.clipboardMonitor1.BackColor = System.Drawing.Color.Red;
+            this.clipboardMonitor1.Location = new System.Drawing.Point(504, 226);
+            this.clipboardMonitor1.Name = "clipboardMonitor1";
+            this.clipboardMonitor1.Size = new System.Drawing.Size(10, 23);
+            this.clipboardMonitor1.TabIndex = 7;
+            this.clipboardMonitor1.Text = "clipboardMonitor1";
+            this.clipboardMonitor1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 261);
+            this.Controls.Add(this.clipboardMonitor1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button button1;
+        private ClipboardMonitor clipboardMonitor1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+
+    }
+}
+
---
+++ YoutubeDownloader/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using YoutubeDownloader.Properties;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            clipboardMonitor1.ClipboardChanged += clipboardMonitor1_ClipboardChanged;
+        }
+
+        private bool onClipBoardDownload = false;
+        void clipboardMonitor1_ClipboardChanged(object sender, ClipboardChangedEventArgs e)
+        {
+            var cliptext = Clipboard.GetText();
+            if (!string.IsNullOrEmpty(cliptext))
+            {
+                if (CheckIfUrl(cliptext) && cliptext.ToLower().Contains("youtube"))
+                {
+                    notifyIcon1.BalloonTipText = "Click here to download " + cliptext;
+                    onClipBoardDownload = true;
+                    notifyIcon1.ShowBalloonTip(3000);
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cliptext = Clipboard.GetText();
+            if (!string.IsNullOrEmpty(cliptext))
+            {
+                if (CheckIfUrl(cliptext))
+                {
+                    txtUrl.Text = cliptext;
+                }
+            }
+            txtDestination.Text = Settings.Default.DownloadPath;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUrl.Text)) return;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private bool CheckIfUrl(string uriName)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        private void RunDownloader()
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "youtube-dl.exe",
+                    Arguments =
+                        txtUrl.Text +
+                        (!string.IsNullOrEmpty(txtDestination.Text)
+                            ? " -o \"" + txtDestination.Text +
+                              "\\%(playlist)s/%(title)s-%(id)s.%(ext)s\""
+                            : ""),
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+
+            proc.Start();
+            var i = 0;
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                var line = proc.StandardOutput.ReadLine();
+                // do something with line
+                backgroundWorker1.ReportProgress(i++, line);
+            }
+
+            if (onClipBoardDownload)
+            {
+                onClipBoardDownload = false;
+                notifyIcon1.BalloonTipText = "File downloaded";
+                notifyIcon1.ShowBalloonTip(2000);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            RunDownloader();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var state = (string) e.UserState;
+            label1.Text = state;
+            progressBar1.Value = GetPercentFromString(state);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtDestination.Text = fbd.SelectedPath;
+                Settings.Default.DownloadPath = fbd.SelectedPath;
+                Settings.Default.Save();
+            }
+        }
+
+        private int GetPercentFromString(string str)
+        {
+            var words = str.Split(' ');
+            var percentage = 0;
+            foreach (var word in words)
+            {
+                if (word.EndsWith("%") && int.TryParse(word.Substring(0, word.Length - 1), out percentage))
+                    break;
+            }
+
+            return percentage;
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            if(!onClipBoardDownload) return;
+            ;
+            var cliptext = Clipboard.GetText();
+            if (!string.IsNullOrEmpty(cliptext))
+            {
+                if (CheckIfUrl(cliptext))
+                {
+                    txtUrl.Text = cliptext;
+                }
+            }
+
+            btnDownload_Click(null, new EventArgs());
+
+        }
+    }
+}
